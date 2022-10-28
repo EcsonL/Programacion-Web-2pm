6b171f0d2daf374158bff3b45c1d9733f3a56c55
Ecson Lumbreras <ecso.1904@gmail.com>
2022-10-27 22:37:44 -0500

    Header

---
+++ Parcial2/Header/Header.js
@@ -0,0 +1,23 @@
+window.onload = function()
+    {
+document.getElementById("btnajax").addEventListener("click", pedirHeader);
+document.getElementById("btnreset").addEventListener("click", iniciarHeader);
+
+function pedirHeader()
+{
+var solicitud = new XMLHttpRequest();
+solicitud.onreadystatechange = function()
+{
+    if(solicitud.readyState == 4 && solicitud.status == 200)
+    {
+        document.getElementById("MyDiv").innerHTML = solicitud.responseText;
+    }
+};
+solicitud.open("GET", "Header.txt", true);
+solicitud.send();
+}
+function iniciarHeader()
+{
+    document.getElementById("MyDiv").innerHTML = "<h2> Este es un header nivel 2<h2>";
+}
+    }
