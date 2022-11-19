57b349ac346562e2fe3b7395aa137fc12ff0c688
Ecson Lumbreras <ecso.1904@gmail.com>
2022-11-19 00:02:02 -0600

    consulta

--- Parcial3/Formulario/consulta.php
+++ Parcial3/Formulario/consulta.php
@@ -9,16 +9,16 @@ $consulta = "select * from empleado";
 
 $registros = mysqli_query($con, $consulta) or die("problemas con el select");
 
-//while ($reg = mysqli_fetch_array($registros, MYSQLI_ASSOC))
-//{
-    //printf($reg['nombre'].''.$reg['apPaterno']);
+while ($reg = mysqli_fetch_array($registros, MYSQLI_ASSOC))
+{
+    printf($reg['nombre'].''.$reg['apPaterno']);
     //id = $reg['nombre'];
     //nombre = $reg['apPaterno'];
     //$registro["id"] = $reg['nombre'];
     //$registro["Nombre"] = $reg['apPaterno'];
 
     //echo json_encode($registro);
-//}
+}
 $reg = mysqli_fetch_array($registros, MYSQLI_ASSOC)
     $registro["id"] = $reg['nombre'];
     $registro["Nombre"] = $reg['apPaterno']
